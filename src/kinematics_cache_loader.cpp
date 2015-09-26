7740de6721846ea6b2724d2479a4f0cc39d53d6e
jlurz24 <jlurz24@gmail.com>
2015-09-26 11:32:25 -0400

    More skeleton

--- src/kinematics_cache_loader.cpp
+++ src/kinematics_cache_loader.cpp
@@ -3,7 +3,10 @@
 namespace {
 using namespace std;
 
+static const double DISCRETIZATION_DEFAULT = 0.01;
+    
 class KinematicsCacheLoader {
+
 private:
 
 	//! Node handle
@@ -17,9 +20,36 @@ public:
 		pnh("~") {
 	}
     
+    double calculateArmLength() {
+        // TODO: Calculate arm length here
+        return 3;
+    }
+    
     void load() {
         ROS_INFO("Loading kinematics cache");
 
+        double discretization;
+        pnh.param<double>("discretization", discretization, DISCRETIZATION_DEFAULT);
+        
+        // Fetch the robot model
+        
+        // Determine length of arm
+        double maxDistance = calculateArmLength();
+        
+        // Iterate through grid. Start at negative x,y,z and iterate
+        // to end at positive x,y,z. All locations are in the frame
+        // of the base of the arm.
+        for (double x = -maxDistance; x <= maxDistance; x += discretization) {
+            for (double y = -maxDistance; y <= maxDistance; y += discretization) {
+                for (double z = -maxDistance; z <= maxDistance; z += discretization) {
+                    ROS_INFO("Attempting to create IK solution at %f, %f, %f", x, y, z);
+                    // TODO: Execute IK
+                    // TODO: Store result in Mongo
+                    // TODO: Store motion plan?
+                    // TODO: Store duration?
+                }
+            }
+        }
         ROS_INFO("Completed loading kinematics cache");
     }
 };
