280c4268a7fa4663ff5f40233541143629005dce
jlurz24 <jlurz24@gmail.com>
2015-11-29 14:15:14 -0500

    Add cache maintenance mode

--- src/kinematics_cache_node.cpp
+++ src/kinematics_cache_node.cpp
@@ -5,6 +5,30 @@
 #include <visualization_msgs/Marker.h>
 
 namespace {
+struct PoseCompare {
+    bool operator() (const geometry_msgs::Pose& a, const geometry_msgs::Pose& b) const {
+        if (a.position.x != b.position.x) {
+            return a.position.x < b.position.x;
+        }
+        if (a.position.y != b.position.y) {
+            return a.position.y < b.position.y;
+        }
+        if (a.position.z != b.position.z) {
+            return a.position.z < b.position.z;
+        }
+        if (a.orientation.x != b.orientation.x) {
+            return a.orientation.x < b.orientation.x;
+        }
+        if (a.orientation.y != b.orientation.y) {
+            return a.orientation.y < b.orientation.y;
+        }
+        if (a.orientation.z != b.orientation.z) {
+            return a.orientation.z < b.orientation.z;
+        }
+        return a.orientation.w < b.orientation.w;
+    }
+};
+
 using namespace std;
 using namespace mongodb_store;
 using namespace mongo;
@@ -33,6 +57,7 @@ private:
 
     //! IK Service
     ros::ServiceServer ikService;
+
 public:
 	KinematicsCacheNode() :
 		pnh("~"), mdb(nh) {
@@ -42,6 +67,14 @@ public:
          allIkPub = nh.advertise<visualization_msgs::Marker>(
 				"/kinematics_cache/known_ik_positions_perm", 1);
 
+        // Detetermine if we should clean on startup
+        bool shouldClean;
+        pnh.param<bool>("clean", shouldClean, false);
+        if(shouldClean) {
+            ROS_INFO("Cleaning database. This may take a few moments.");
+            clean();
+        }
+
         ikService = nh.advertiseService("/kinematics_cache/ik",
             &KinematicsCacheNode::query, this);
 	}
@@ -49,7 +82,7 @@ public:
 private:
     void visualize(const string group, const double maxTime = 60.0) {
         vector<boost::shared_ptr<kinematics_cache::IK> > ikPositions = queryAll(group, maxTime);
-        ROS_INFO("Found %lu results", ikPositions.size());
+        ROS_DEBUG("Found %lu results", ikPositions.size());
 
         visualization_msgs::Marker points;
         points.header.frame_id = baseFrame;
@@ -104,9 +137,77 @@ private:
         return true;
     }
 
+    bool withinResolution(double a, double b) const {
+        return fabs(a - b) < resolution / 2.0;
+    }
+
+    bool posesEqual(const geometry_msgs::Pose& a, const geometry_msgs::Pose& b) const {
+
+        return withinResolution(a.position.x, b.position.x) &&
+               withinResolution(a.position.y, b.position.y) &&
+               withinResolution(a.position.z, b.position.z) &&
+               withinResolution(a.orientation.x, b.orientation.x) &&
+               withinResolution(a.orientation.y, b.orientation.y) &&
+               withinResolution(a.orientation.z, b.orientation.z) &&
+               withinResolution(a.orientation.w, b.orientation.w);
+    }
+
+    void clean() {
+        ROS_INFO("Cleaning the IK cache.");
+        // Brute force. Search all and load multimap.
+
+        // Clean one group at a time
+        const string groups[] = {"left_arm", "right_arm"};
+        for (unsigned int i = 0; i < boost::size(groups); ++i) {
+            ROS_INFO("Cleaning group %s", groups[i].c_str());
+            multimap<geometry_msgs::Pose, string, PoseCompare> poses;
+            vector< std::pair<boost::shared_ptr<kinematics_cache::IK>, mongo::BSONObj> > results;
+
+            BSONObjBuilder b;
+            b.append("group", groups[i]);
+
+            mongo::BSONObj query = b.obj();
+            mongo::BSONObj metaDataQuery;
+
+            ROS_INFO_STREAM("Executing query: " << query);
+            if (mdb.query<kinematics_cache::IK>(results, query, metaDataQuery, false)) {
+                ROS_INFO("Query for all entries succeeded");
+            } else {
+                ROS_ERROR("Query for all entries failed");
+                return;
+            }
+
+            // Create a map from pose to id
+            ROS_INFO("Loading the multimap with %lu results", results.size());
+            for (unsigned int j = 0; j < results.size(); ++j) {
+                // Extract object ID
+                string objectId = results[j].second["_id"].OID().toString();
+                poses.insert(make_pair(results[j].first->pose.pose, objectId));
+            }
+            ROS_INFO("Multimap loaded successfully");
+
+            geometry_msgs::Pose lastPose;
+            vector<string> idsToDelete;
+            ROS_INFO("Searching for duplicates");
+            for (multimap<geometry_msgs::Pose, string>::iterator iter = poses.begin(); iter != poses.end(); ++iter) {
+                if (posesEqual(lastPose, iter->first)) {
+                    ROS_INFO("Found duplicate pose. Adding id %s to delete", iter->second.c_str());
+                    idsToDelete.push_back(iter->second);
+                }
+                lastPose = iter->first;
+            }
+            ROS_INFO("Duplicate search completed. Found %lu entries to delete", idsToDelete.size());
+
+            for (vector<string>::iterator id = idsToDelete.begin(); id != idsToDelete.end(); ++id) {
+                mdb.deleteID(*id);
+            }
+            ROS_INFO("Completed deleting entries. Completed cleaning cache for group");
+        }
+    }
+
     boost::shared_ptr<kinematics_cache::IK> query(const std::string group,
         const geometry_msgs::PoseStamped pose) {
-        // Do not perform IK, as it is a footgun for performance
+        // Do not perform transform, as it is a footgun for performance
         if (pose.header.frame_id != baseFrame) {
             ROS_ERROR("Queries must be specified in the base frame");
             return boost::shared_ptr<kinematics_cache::IK>();
