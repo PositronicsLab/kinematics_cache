f00c8502ad91313b4bb641379d72d78c8278d668
jlurz24 <jlurz24@gmail.com>
2015-12-22 22:51:29 -0500

    Separate left/right arm

--- src/execution_time_estimator.cpp
+++ src/execution_time_estimator.cpp
@@ -34,12 +34,15 @@ private:
 
     //! Number of trials
     int trials;
+
+    //! Arm
+    string armName;
 public:
 	ExecutionTimeEstimator() :
 		pnh("~"), mdb(nh) {
          pnh.param<int>("trialsPerConfiguration", trialsPerConfiguration, 3);
          pnh.param<int>("trials", trials, 10);
-        updateExecutionTimes();
+         pnh.param<string>("arm", armName, "left_arm");
 	}
 
 private:
@@ -53,102 +56,115 @@ private:
         return d;
     }
 
-    void updateExecutionTimes() {
+    bool moveArm(ArmClient& client, const human_catching::MoveArmFastGoal& goal) {
+        client.sendGoal(goal);
+        if (client.waitForResult(ros::Duration(60.0))) {
+            actionlib::SimpleClientGoalState state = client.getState();
+            ROS_INFO("Action finished: %s", state.toString().c_str());
+        }
+        else {
+            ROS_INFO("Action did not finish before the time out.");
+        }
+    }
+
+    public:
+        void update() {
         ROS_INFO("Updating the execution times");
 
-        // Update one group at a time
-        const string groups[] = {"left_arm", "right_arm"};
-        for (unsigned int i = 0; i < boost::size(groups); ++i) {
-            moveit::planning_interface::MoveGroup group(groups[i]);
+        moveit::planning_interface::MoveGroup group(armName);
 
-            ArmClient arm(groups[i] + "_move_arm_fast_action_server", true);
-            ROS_INFO("Waiting for %s", (groups[i] + "_move_arm_fast_action_server").c_str());
-            arm.waitForServer();
+        ArmClient arm(armName + "_move_arm_fast_action_server", true);
+        ROS_INFO("Waiting for %s", (armName + "_move_arm_fast_action_server").c_str());
+        arm.waitForServer();
 
-            ROS_INFO("Updating group %s", groups[i].c_str());
+        ROS_INFO("Updating group %s", armName.c_str());
 
-            ROS_INFO("Resetting arm to zero position");
-            human_catching::MoveArmFastGoal baseGoal;
-            baseGoal.joint_positions = vector<double>(7);
-            arm.sendGoal(baseGoal);
+        ROS_INFO("Resetting arm to zero position");
+        human_catching::MoveArmFastGoal baseGoal;
+        baseGoal.joint_positions = vector<double>(7);
+        moveArm(arm, baseGoal);
 
-            // TODO: Move other arm out of the way.
+        // Collisions are disabled so the other arm cannot interfere
 
-            ROS_INFO("Querying for all records");
-            BSONObjBuilder b;
-            b.append("group", groups[i]);
+        ROS_INFO("Querying for all records");
+        BSONObjBuilder b;
+        b.append("group", armName);
 
-            mongo::BSONObj query = b.obj();
-            mongo::BSONObj metaDataQuery;
+        mongo::BSONObj query = b.obj();
+        mongo::BSONObj metaDataQuery;
 
-            ROS_DEBUG_STREAM("Executing query: " << query);
-            IKResultsList results;
-            if (mdb.query<kinematics_cache::IK>(results, query, metaDataQuery, false)) {
-                ROS_INFO("Query for all entries succeeded");
-            } else {
-                ROS_ERROR("Query for all entries failed");
-                return;
-            }
-
-            for (unsigned int i = 0; i < trials; ++i) {
-                ROS_INFO("Beginning trial %u", i);
-
-                // Initialize to random pose
-                // TODO: Use a resolution complete method
-                vector<double> randomJointAngles = group.getRandomJointValues();
-
-                // Simulate the movement and average the time
-                ros::Duration totalTime(0);
-                for (unsigned int i = 0; i < trialsPerConfiguration; ++ i) {
-
-                    // Move the arm
-                    ROS_INFO("Moving arm to random position.");
-                    human_catching::MoveArmFastGoal goal;
-                    goal.joint_positions = randomJointAngles;
-                    ros::Time begin = ros::Time::now();
-                    if (!arm.sendGoal(goal)) {
-                        ROS_WARN("Failed to reach goal");
-                        // TODO: Error handling
-                    }
-                    ros::Time end = ros::Time::now();
-                    ROS_INFO("Arm movement completed");
-
-                    // Calculate duration
-                    totalTime += (end - begin);
-
-                    // Reset the arm to the zero configuration
-                    ROS_INFO("Resetting arm to zero position");
-                    arm.sendGoal(baseGoal);
-                    ROS_INFO("Reset completed");
-                }
-
-                // Average the time
-                ros::Duration timePerTrial(totalTime.toSec() / trialsPerConfiguration);
-
-                ROS_INFO("Simulated estimated is %f", timePerTrial.toSec());
-
-                // Update any configurations where the total joint distance is less than for the current estimate
-                for (IKResultsList::iterator iter = results.begin(); iter != results.end(); ++iter) {
-                    double currDistance = distance(iter->first->positions, randomJointAngles);
-                    ROS_DEBUG("Distance to current configuration is %f", currDistance);
-                    if (currDistance < iter->first->minimum_distance) {
-                        ROS_DEBUG("Updating due to %f lower than %f. Calculated estimate was %f.", currDistance,
-                                 iter->first->minimum_distance, iter->first->execution_time.toSec());
-                        boost::shared_ptr<kinematics_cache::IK> msg = iter->first;
-                        msg->minimum_distance = currDistance;
-                        msg->simulated_execution_time = timePerTrial;
-                    }
-                }
-            }
-
-            // Now perform final updates
-            ROS_INFO("Updating records");
-            for (IKResultsList::iterator iter = results.begin(); iter != results.end(); ++iter) {
-                mdb.updateID(iter->second["_id"].OID().toString(), *iter->first);
-            }
-            ROS_INFO("Completed updating entries. Completed updating execution time for group");
+        ROS_DEBUG_STREAM("Executing query: " << query);
+        IKResultsList results;
+        if (mdb.query<kinematics_cache::IK>(results, query, metaDataQuery, false)) {
+            ROS_INFO("Query for all entries succeeded");
+        } else {
+            ROS_ERROR("Query for all entries failed");
+            return;
         }
-        ROS_INFO("All updates complete");
+
+        for (unsigned int i = 0; i < trials; ++i) {
+            ROS_INFO("Beginning trial %u", i);
+
+            // Initialize to random pose
+            // TODO: Use a resolution complete method
+            vector<double> randomJointAngles = group.getRandomJointValues();
+
+            // Simulate the movement and average the time
+            ros::Duration totalTime(0);
+            unsigned int successfulTrials = 0;
+            for (unsigned int i = 0; i < trialsPerConfiguration; ++ i) {
+
+                // Move the arm
+                ROS_INFO("Moving arm to random position.");
+                human_catching::MoveArmFastGoal goal;
+                goal.joint_positions = randomJointAngles;
+                ros::Time begin = ros::Time::now();
+                if (!moveArm(arm, goal)){
+                    continue;
+                }
+                successfulTrials++;
+                ros::Time end = ros::Time::now();
+                ROS_INFO("Arm movement completed");
+
+                // Calculate duration
+                totalTime += (end - begin);
+
+                // Reset the arm to the zero configuration
+                ROS_INFO("Resetting arm to zero position");
+                moveArm(arm, baseGoal);
+                ROS_INFO("Reset completed");
+            }
+
+            if (successfulTrials < 2) {
+                ROS_WARN("Could not execute path successfully a minimum number of times. Skipping trial");
+                continue;
+            }
+
+            // Average the time
+            ros::Duration timePerTrial(totalTime.toSec() / successfulTrials);
+
+            ROS_INFO("Simulated estimated is %f", timePerTrial.toSec());
+
+            // Update any configurations where the total joint distance is less than for the current estimate
+            for (IKResultsList::iterator iter = results.begin(); iter != results.end(); ++iter) {
+                double currDistance = distance(iter->first->positions, randomJointAngles);
+                ROS_DEBUG("Distance to current configuration is %f", currDistance);
+                if (currDistance < iter->first->minimum_distance) {
+                    ROS_DEBUG("Updating due to %f lower than %f. Calculated estimate was %f.", currDistance,
+                                iter->first->minimum_distance, iter->first->execution_time.toSec());
+                    boost::shared_ptr<kinematics_cache::IK> msg = iter->first;
+                    msg->minimum_distance = currDistance;
+                    msg->simulated_execution_time = timePerTrial;
+                }
+            }
+        }
+
+        // Now perform final updates
+        ROS_INFO("Updating records");
+        for (IKResultsList::iterator iter = results.begin(); iter != results.end(); ++iter) {
+            mdb.updateID(iter->second["_id"].OID().toString(), *iter->first);
+        }
+        ROS_INFO("Completed updating entries. Completed updating execution time for group");
     }
 };
 }
@@ -156,5 +172,5 @@ private:
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "execution_time_estimator");
 	ExecutionTimeEstimator ete;
-	ros::spin();
+	ete.update();
 }
